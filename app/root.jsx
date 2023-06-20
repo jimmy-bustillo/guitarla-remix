e34d197d8f8b739b2238654c3e28c771fa761feb
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-06-19 21:46:01 -0600

    Agrega los estilos

--- app/root.jsx
+++ app/root.jsx
@@ -1,11 +1,15 @@
-import { Meta } from "@remix-run/react"
-
+import { Meta, Links } from "@remix-run/react"
+import styles from "./styles/index.css"
 export function meta() {
   return [
     { charset: "utf-8" },
     { title: "GuitarLA - Remix" },
     { name: "viewport", content: "width=device-width,initial-scale=1" },
   ]
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
 }
 
 export default function App() {
@@ -21,6 +25,7 @@ function Document({ children }) {
     <html lang='es'>
       <head>
         <Meta />
+        <Links />
       </head>
       <body>{children}</body>
     </html>
