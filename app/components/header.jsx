2c9779dcd2544e3fb415332a41826b44e53c3611
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-06-21 21:24:04 -0600

    Añade el logotipo

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,16 +1,18 @@
 import { Link } from "@remix-run/react"
+import logo from "../../public/img/logo.svg"
 
 function Header() {
   return (
     <header className='header'>
       <div className='contenedor barra'>
-        <div className='logo'></div>
+        <Link to='/'>
+          <img className='logo' src={logo} alt='Imagen logo' />
+        </Link>
         <nav className='navegacion'>
           <Link to='/'>Inicio</Link>
           <Link to='/nosotros'>Nosotros</Link>
           <Link to='/tienda'>Tienda</Link>
           <Link to='/blog'>Blog</Link>
-
         </nav>
       </div>
     </header>
