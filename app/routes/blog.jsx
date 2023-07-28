52c3d9ac9d2ca1cf1d3c6165b1f1e4ff6c3a7deb
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-07-27 23:26:45 -0600

    Añade las entradas de blog

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,7 +1,29 @@
+import { getPosts } from "~/models/post.server"
+import { useLoaderData } from "@remix-run/react"
+import Post from "../components/post"
+
+export async function loader() {
+  const posts = await getPosts()
+  console.log(posts);
+  return posts.data
+}
 
 function Blog() {
+  const posts = useLoaderData()
+
   return (
-    <div>Desde blog</div>
+    <main className='contenedor'>
+      <h2 className='heading'>Blog</h2>
+      <div className='blog'>
+      
+        {posts.map(post => (
+          <Post 
+            key={post.id}
+            post={post.attributes}
+          />
+        ))}
+      </div>
+    </main>
   )
 }
 
