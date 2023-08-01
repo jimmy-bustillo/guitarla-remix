3f8cd812620397e984278a3b4c032de6470aa7db
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-07-31 21:47:12 -0600

    Añade hoja de estilo para el blog

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,10 +1,15 @@
 import { getPosts } from "~/models/post.server"
 import { useLoaderData } from "@remix-run/react"
 import Post from "../components/post"
+import styles from "~/styles/blog.css"
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
+}
 
 export async function loader() {
   const posts = await getPosts()
-  console.log(posts);
+  console.log(posts)
   return posts.data
 }
 
@@ -15,12 +20,8 @@ function Blog() {
     <main className='contenedor'>
       <h2 className='heading'>Blog</h2>
       <div className='blog'>
-      
-        {posts.map(post => (
-          <Post 
-            key={post.id}
-            post={post.attributes}
-          />
+        {posts.map((post) => (
+          <Post key={post.id} post={post.attributes} />
         ))}
       </div>
     </main>
