096f5c588634e24690ee8b31c50b5b5af4c7b652
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-01 21:57:30 -0600

    Obtiene el post desde la url con routing dinamico

--- app/models/post.server.js
+++ app/models/post.server.js
@@ -3,3 +3,10 @@ export async function getPosts() {
 
   return (resultado = await respuesta.json())
 }
+
+export async function getPost(url) {
+  const respuesta = await fetch(
+    `${process.env.API_URL}/posts?filters[url]=${url}&populate=imagen`
+  )
+  return await respuesta.json()
+}
