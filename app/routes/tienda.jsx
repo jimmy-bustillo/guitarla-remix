b4e370177e6839722fc9de6409d194a5a5fb21d7
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-02 16:28:16 -0600

    Cambia el listado de guitarras por componente

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "@remix-run/react"
 import { getGuitarras } from "~/models/guitarras.server"
-import Guitarra from "~/components/guitarra"
+import ListadoGuitarras from "../components/listado-guitarras"
 import styles from "~/styles/guitarras.css"
 
 export function meta() {
@@ -24,15 +24,7 @@ function Tienda() {
   const guitarras = useLoaderData()
   return (
     <main className='contendor'>
-      <h2 className='heading'>Nuestra Colección</h2>
-
-      {guitarras.length && (
-        <div className='guitarras-grid'>
-          {guitarras.map((guitarra) => (
-            <Guitarra key={guitarra?.id} guitarra={guitarra?.attributes} />
-          ))}
-        </div>
-      )}
+      <ListadoGuitarras guitarras={guitarras} />
     </main>
   )
 }
