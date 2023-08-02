f2aec170bea156b3adb1c8337285a5b3f8a04dc8
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-02 16:27:22 -0600

    Añáde la sección de guitarras

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,6 +1,39 @@
+import { useLoaderData } from "@remix-run/react"
+import { getGuitarras } from "~/models/guitarras.server"
+import { getPosts } from "~/models/posts.server"
+import ListadoGuitarras from '~/components/listado-guitarras'
+import styleGuitarras from '~/styles/guitarras.css'
+
+export function meta() {}
+
+export function links() {
+  return[
+    {
+      rel: 'stylesheet',
+      href: styleGuitarras
+    }
+  ]
+}
+
+export async function loader() {
+  const [guitarras, posts] = await Promise.all([getGuitarras(), getPosts()])
+
+  return {
+    guitarras: guitarras.data,
+    posts: posts.data,
+  }
+}
+
 function Index() {
+  const { guitarras, posts } = useLoaderData()
   return (
-    <div>Desde Index.jsx</div>
+    <>
+      <main className='contenedor'>
+        <ListadoGuitarras 
+          guitarras={guitarras}
+        />
+      </main>
+    </>
   )
 }
 
