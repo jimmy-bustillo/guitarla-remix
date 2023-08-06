cf5e8f7251f6c1bd1b7a5099b260695974d7295c
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-05 21:29:14 -0600

    Agrega useState y validación para seleccionar la cantidad

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useLoaderData } from "@remix-run/react"
 import { getGuitarra } from "~/models/guitarras.server"
 
@@ -26,8 +27,27 @@ export async function loader({ params }) {
 }
 
 function Guitarra() {
+  const [cantidad, setCantidad] = useState(0)
   const guitarra = useLoaderData()
   const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (cantidad < 1) {
+      alert("Debes selccionar una cantidad")
+      return
+    }
+
+    const guitarraSeleccionada = {
+      id: guitarra.data[0].id,
+      imagen: imagen.data.attributes.url,
+      nombre,
+      precio,
+      cantidad
+    }
+
+  }
 
   return (
     <div className='guitarra'>
@@ -42,10 +62,10 @@ function Guitarra() {
         <p className='texto'>{descripcion}</p>
         <p className='precio'>${precio}</p>
 
-        <form className='formulario'>
-          <label htmlFor="cantidad">Cantidad</label>
-          <select id="cantidad">
-            <option value=''>-- Seleccione --</option>
+        <form onSubmit={handleSubmit} className='formulario'>
+          <label htmlFor='cantidad'>Cantidad</label>
+          <select onChange={(e) => setCantidad(+e.target.value)} id='cantidad'>
+            <option value='0'>-- Seleccione --</option>
             <option value='1'>1</option>
             <option value='2'>2</option>
             <option value='3'>3</option>
@@ -53,10 +73,7 @@ function Guitarra() {
             <option value='5'>5</option>
           </select>
 
-          <input 
-            type="submit"
-            value="Agregar al carrito"
-          />
+          <input type='submit' value='Agregar al carrito' />
         </form>
       </div>
     </div>
