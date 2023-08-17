9f82c5f61cf6ad5ed4b1d609b1d725e1771804e4
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-16 21:33:18 -0600

    Verifica si hay elementos duplicados en el carrito

--- app/root.jsx
+++ app/root.jsx
@@ -52,7 +52,21 @@ export default function App() {
   const [carrito, setCarrito] = useState([])
 
   const agregarCarrito = (guitarra) => {
-    setCarrito([...carrito, guitarra])
+    if(carrito.some(guitarraState => guitarraState.id === guitarra.id)){
+      // Iterar sobre el arreglo e identificar el elemento duplicado
+      const carritoActualizado = carrito.map(guitarraState => {
+        if(guitarraState.id === guitarra.id){
+          // Reescribir la cantidad
+          guitarraState.cantidad = guitarra.cantidad
+        }
+        return guitarraState
+      })
+      // Añadir al carrito
+      setCarrito(carritoActualizado)
+    } else{
+      // Registro nuevo, agregar al carrito
+      setCarrito([...carrito, guitarra])
+    }
   }
 
   return (
