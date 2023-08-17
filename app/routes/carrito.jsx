b79eec16bdd8629135f6e181f23f15f61760717b
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-16 21:46:22 -0600

    Muestra los productos agregados al carrito

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,3 +1,5 @@
+import { useOutletContext } from "@remix-run/react"
+
 import styles from "~/styles/carrito.css"
 
 export function links() {
@@ -20,12 +22,37 @@ export function meta() {
 }
 
 export default function Carrito() {
+  const { carrito } = useOutletContext()
   return (
     <main className='contenedor'>
       <h1 className='heading'>Carrito de compras</h1>
       <div className='contenido'>
         <div className='carrito'>
           <h2>Articulos</h2>
+          {carrito.length === 0
+            ? "Carrito Vacio"
+            : carrito.map((producto) => (
+                <div key={producto.id} className='producto'>
+                  <div>
+                    <img
+                      className='imagen'
+                      src={producto.imagen}
+                      alt={`Imagen del producto ${producto.nombre}`}
+                    />
+                  </div>
+
+                  <div>
+                    <p className='nombre'>{producto.nombre}</p>
+
+                    <p className='precio'>
+                      $<span>{producto.precio}</span>
+                    </p>
+                    <p className='subtotal'>
+                      SubTotal: $<span>{producto.cantidad * producto.precio}</span>
+                    </p>
+                  </div>
+                </div>
+              ))}
         </div>
 
         <aside className='resumen'>
