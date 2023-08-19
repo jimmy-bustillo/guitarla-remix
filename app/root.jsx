6fa0d3856eabfaad7fd503e7d45e55b78565ca0c
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-18 21:29:34 -0600

    Agrega funcoión para eliminar productos del carrito

--- app/root.jsx
+++ app/root.jsx
@@ -80,6 +80,11 @@ export default function App() {
     setCarrito(carritoActualizado)
   }
 
+  const eliminarGuitarra = id => {
+    const carritoActualizado = carrito.filter(guitarraState => guitarraState.id !== id)
+    setCarrito(carritoActualizado)
+  }
+
   return (
     <Document>
       <Outlet
@@ -87,6 +92,7 @@ export default function App() {
           agregarCarrito,
           carrito,
           actualizarCantidad,
+          eliminarGuitarra
         }}
       />
     </Document>
